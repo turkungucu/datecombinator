97c67a37b19cce9ee1ea80710339222946c4a305
Emre Colak <colemre@gmail.com>
2011-06-30 22:14:50 -0700

    checking in the csv file for zip codes

--- lib/tasks/import_zip_codes.rb
+++ lib/tasks/import_zip_codes.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 puts "Starting parsing csv file"
 count = 0;
-CSV.foreach("C:/Users/ecolak/Downloads/zipnov99.csv") do |row|
+CSV.foreach("zipnov99.csv") do |row|
   zip_code = ZipCode.new
   zip_code.zip = row[0]
   zip_code.latitude = row[1].to_f
@@ -10,6 +10,7 @@ CSV.foreach("C:/Users/ecolak/Downloads/zipnov99.csv") do |row|
   zip_code.city = row[4]
   zip_code_state = row[5]
   zip_code.save
+  
   count += 1
   if count % 1000 == 0
     puts "Total rows inserted: #{count}"
