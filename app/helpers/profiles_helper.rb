201b18fd7e847d06c9a523466c82fd75c5e79410
Alinur Goksel <alinurg@yahoo.com>
2011-07-01 19:28:31 -0700

    agoksel: extracting out hardcoded values as helper methods

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,15 +1,29 @@
 module ProfilesHelper
+  # calculate age given date of birth
   def age(dob)
     now = Time.now.utc.to_date
     now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
   end
   
-  def cityFromZip(zip)
+  # get the name of the city that given zipcode belongs to
+  def city_from_zip(zip)
     zc = ZipCode.find_by_zip(zip)
     if zc.nil?
       return ""
     else
       return zc.city
     end
+  end
+  
+  # to be used in gender dropdowns
+  # ex:
+  #  <%= f.select :seeking_gender, options_for_select(gender_list) %>
+  def gender_list
+    [['Male', 'male'], ['Female', 'female']]
+  end
+  
+  # number of characters displayed as profile description on search results
+  def desc_char_limit
+    350
   end
 end
