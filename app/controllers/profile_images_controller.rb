cff1020ff76c4c52f0e2f600112d52b05e91773b
Alinur Goksel <alinurg@yahoo.com>
2011-07-06 00:48:30 -0700

    agoksel: adding delete functionality to profile images and urls

--- app/controllers/profile_images_controller.rb
+++ app/controllers/profile_images_controller.rb
@@ -6,4 +6,16 @@ class ProfileImagesController < ApplicationController
     @profile_image = @profile.profile_images.create(params[:profile_image])
     redirect_to profile_path(@profile)
   end
+  
+  def destroy
+    @profile = params[:profile_id]
+    @profile_image = ProfileImage.find(params[:id])
+    @profile_image.destroy
+
+    respond_to do |format|
+      format.html { redirect_to profile_url(@profile) }
+      format.json { head :ok }
+    end
+  end
+  
 end
--- app/controllers/profile_urls_controller.rb
+++ app/controllers/profile_urls_controller.rb
@@ -6,4 +6,15 @@ class ProfileUrlsController < ApplicationController
     @profile_url = @profile.profile_urls.create(params[:profile_url])
     redirect_to profile_path(@profile)
   end
+  
+  def destroy
+    @profile = params[:profile_id]
+    @profile_url = ProfileUrl.find(params[:id])
+    @profile_url.destroy
+
+    respond_to do |format|
+      format.html { redirect_to profile_url(@profile) }
+      format.json { head :ok }
+    end
+  end
 end
