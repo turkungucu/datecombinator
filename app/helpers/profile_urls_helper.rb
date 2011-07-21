84a53b00f60d225b572d2b3a5d907b0dc5271d74
Alinur Goksel <alinurg@yahoo.com>
2011-07-20 19:40:58 -0700

    agoksel: adding sample url label for clickout button to be used in demo

--- app/helpers/profile_urls_helper.rb
+++ app/helpers/profile_urls_helper.rb
@@ -1,2 +1,8 @@
 module ProfileUrlsHelper
+  # to be used in url display name dropdowns
+  # ex:
+  #  <%= f.select :display_name, options_for_select(url_label_list) %>
+  def url_label_list
+    [['Your Website', 'Your Website'], ['PlentyOfFish', 'PlentyOfFish'], ['Match.com', 'Match.com']]
+  end
 end
